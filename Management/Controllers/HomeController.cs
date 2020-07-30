81ae40951b2e0d7d1cfb644043ef2e270b8dbb14
Marco Giorgio Burtini <marcogiorgio.burtini@studenti.unipd.it>
2020-07-30 10:44:38 +0200

    Rework

--- Management/Controllers/HomeController.cs
+++ Management/Controllers/HomeController.cs
@@ -9,12 +9,16 @@ using Management.Models;
 using Microsoft.AspNetCore.Hosting;
 using System.IO;
 using Utils;
+using System.Net.Http;
+
 namespace Management.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
         private static IWebHostEnvironment _hostingEnvironment;
+        private static Config _config;
+        private readonly IHttpClientFactory _clientFactory;
 
         public HomeController(ILogger<HomeController> logger, IWebHostEnvironment hostEnvironment)
         {
@@ -38,12 +42,46 @@ namespace Management.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
+        public string CheckAll()
+        {
+            string status = "";
+            foreach (Modulo m in _config.Monitoring.Modules)
+                status += CheckAvailability(m);
+
+            // rimuoviamo l'ultimo ";"
+            return status.Remove(status.Length - 1, 1);
+        }
+
      
-        public JsonResult CheckAvailability(String id)
+        public async string CheckAvailability(Modulo modulo)
         {
             var path = Path.Combine(_hostingEnvironment.WebRootPath, "config.json");
 
-            return null;
+            // creiamo il client che contatterà il modulo esterno
+            var client = _clientFactory.CreateClient();
+
+            // prepariamo la risposta
+            var request = new HttpRequestMessage(HttpMethod.Get,
+            $"http://{modulo.Ip}:{modulo.Port}");
+            request.Headers.Add("Accept", "application/vnd.github.v3+json");
+            request.Headers.Add("User-Agent", "HttpClientFactory-Sample");
+            
+            // inviamo la richiesta
+            var response = await client.SendAsync(request);
+
+            // analizziamo la risposta
+            try
+            {
+                if (response.IsSuccessStatusCode)
+                    return $"{modulo.Name}:1;";
+                else
+                    return $"{modulo.Name}:0;";        
+            }
+            catch (Exception e)
+            {
+                return $"{modulo.Name}:0;";
+            }
+
         }
     }
 }
